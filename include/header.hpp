7cb0f1169afbd79bc516c30b0258fd9b3775b991
Sergei2000 <dsdem@mail.ru>
2019-12-17 13:21:46 +0300

    final 2

--- include/header.hpp
+++ include/header.hpp
@@ -44,7 +44,7 @@ explicit  SharedPtr(const SharedPtr& r)
             delete(_adress);
         }
     }
-explicit SharedPtr(SharedPtr&& r )
+SharedPtr(SharedPtr&& r )
 {_adress = r._adress; r._adress = nullptr;}
    auto operator=(SharedPtr& r) -> SharedPtr&
    {
